7a0b41c9d55facbbfb866f6dece73e0b24858db8
coucou <1131088589@qq.com>
2019-11-07 22:47:21 +0800

    11.7

--- day05_mybatis_one2many/src/main/java/com/dao/AccountDao.java
+++ day05_mybatis_one2many/src/main/java/com/dao/AccountDao.java
@@ -1,10 +1,16 @@
 package com.dao;
 
 import com.pojo.Account;
+import com.pojo.AccountUser;
 
 import java.util.List;
 
 public interface AccountDao {
 
     List<Account> findAll();
+
+    /**
+     * 查询所有账户，并且带有账户所对应的用户信息
+     */
+    List<AccountUser> findAllAccount();
 }
--- day05_mybatis_one2many/src/test/java/com/test/AccountTest.java
+++ day05_mybatis_one2many/src/test/java/com/test/AccountTest.java
@@ -3,6 +3,7 @@ package com.test;
 
 import com.dao.AccountDao;
 import com.pojo.Account;
+import com.pojo.AccountUser;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
@@ -17,8 +18,20 @@ public class AccountTest extends BaseTest {
 
         List<Account> list = accountDao.findAll();
         printList(list );
-
     }
+
+
+    @Test
+    public void testFindAccountAndUser(){
+        SqlSession session = getSqlSession();
+        AccountDao accountDao = session.getMapper(AccountDao.class);
+
+        List<AccountUser> list = accountDao.findAllAccount();
+        for(AccountUser a:list){
+            System.out.println(a);
+        }
+    }
+
 
 
     private void printList(List<Account> list){
