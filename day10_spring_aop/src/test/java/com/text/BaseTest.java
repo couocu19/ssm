2221a0eb0038d45cca9746fb46173860ab094344
coucou <1131088589@qq.com>
2019-11-18 19:21:26 +0800

    11.18

---
+++ day10_spring_aop/src/test/java/com/text/BaseTest.java
@@ -0,0 +1,26 @@
+package com.text;
+
+
+import com.service.AccountService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BaseTest {
+
+    @Test
+    public void testAdd(){
+
+        //获取核心容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //得到业务层对象
+        AccountService as = ac.getBean("accountService",AccountService.class);
+
+
+        as.addAccount();
+        
+    }
+
+
+
+}
