8579c2ef76f2f62fc19dd4fd14987e981c76ee75
coucou <1131088589@qq.com>
2019-11-23 20:52:31 +0800

    11.23

--- day12_springmvc_init/src/main/java/com/controller/AnnoControler.java
+++ day12_springmvc_init/src/main/java/com/controller/AnnoControler.java
@@ -1,4 +1,22 @@
 package com.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("/anno")
 public class AnnoControler {
+
+    @RequestMapping("/testRequestparam")
+    public String testRequestparam(String username){
+
+        System.out.println("执行了……");
+        System.out.println(username);
+        return "syccess";
+    }
+
+
+
+
 }
--- day12_springmvc_init/src/main/java/com/utils/StringToDate.java
+++ day12_springmvc_init/src/main/java/com/utils/StringToDate.java
@@ -1,4 +1,36 @@
 package com.utils;
 
-public class StringToDate {
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ *把字符串转换为日期
+ */
+public class StringToDate implements Converter<String, Date>{
+
+    /**
+     * 传入一个字符串并将其转换为Date类型
+     * @param s
+     * @return
+     */
+    @Override
+    public Date convert(String s) {
+        if(s == null){
+            throw new RuntimeException("请您传入数据");
+        }
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return df.parse(s);
+        } catch (Exception e) {
+            throw new RuntimeException("数据类型转换出现错误");
+        }
+
+    }
 }
